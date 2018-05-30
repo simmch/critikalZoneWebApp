77dcc35e7132f4de60d3e2874cd96f21d9f0a890
simmch <Chris@Chriss-iMac.local>
2018-05-29 20:11:59 -0400

    New updates for uploading file part 1

--- src/actions/articleAction.js
+++ src/actions/articleAction.js
@@ -1,6 +1,7 @@
 import {GET_ART} from '../actionTypes';
 import { database } from '../firebase/firebase';
 
+// article element comes from articleCreator
 export function saveArticle(article){
     return dispatch => database.push(article);
 }
--- src/component/admin/content/articleCreator.js
+++ src/component/admin/content/articleCreator.js
@@ -4,6 +4,9 @@ import { saveArticle } from '../../../actions/articleAction';
 
 import { connect } from 'react-redux';
 
+//////////////////////
+// ADMIN CLASS ONLY //
+//////////////////////
 class ArticleCreator extends Component { 
 
   constructor(props){
@@ -17,10 +20,11 @@ class ArticleCreator extends Component {
       articleImage: '',
       isUploading: false,
       progress: 0,
-      body: ''
+      body: '',
+      img: {}
     };
   
-    // bind this functions
+    // bind functions
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -35,11 +39,13 @@ class ArticleCreator extends Component {
 
   handleSubmit(e){
       e.preventDefault();
+      // Takes the current property state and pushes it to database
       const article = {
           header: this.state.header,
           tagLine: this.state.tagLine,
           articleType: this.state.articleType,
-          system: this.state.system
+          system: this.state.system,
+          body: this.state.body
       }
     this.props.saveArticle(article);
   }
@@ -53,6 +59,7 @@ class ArticleCreator extends Component {
               <div className="col-sm-8">
                 
                 <form onSubmit={this.handleSubmit}>
+                  {/* Header */}
                   <div className="form-group">
                   <label htmlFor="header">Header: </label>
                   <input 
@@ -65,7 +72,7 @@ class ArticleCreator extends Component {
                     required
                     />
                     </div>
-      
+                    {/* TagLine */}
                     <div className="form-group">
                     <label htmlFor="tagLine">Tag Line: </label>
                     <input  
@@ -78,7 +85,7 @@ class ArticleCreator extends Component {
                       required
                     />
                     </div>
-
+                    {/* Article Type */}
                     <div className="form-group">
                     <label htmlFor="articleType">Article Type</label>
                     <select required multiple className="form-control" name="articleType" onChange={this.handleChange}>
@@ -86,7 +93,7 @@ class ArticleCreator extends Component {
                     <option  value="Review">Review</option>
                     </select>
                     </div>
-
+                    {/* System */}
                     <div className="form-group">
                     <label htmlFor="system">System</label>
                     <select required multiple className="form-control" name="system" onChange={this.handleChange}>
@@ -95,6 +102,29 @@ class ArticleCreator extends Component {
                     <option value="Switch">Switch</option>
                     <option value="PC">PC</option>
                     </select>
+                    </div>
+                    {/* Body */}
+                    <div className="form-group">
+                    <label htmlFor="body">Body: </label>
+                    <textarea className="form-control" 
+                    required
+                    name="body"
+                    onChange={this.handleChange}
+                    value={this.state.body}
+                    />
+                    {/* img */}
+                    <br />
+                    <div className="input-group mb-3 form-group">
+                      <div className="custom-file">
+                        <label class="custom-file-label upload-group" for="img">Choose file</label>
+                        <input type="file" className="custom-file-input" id="img"/>
+                      </div>
+                      {/* <div className="input-group-append">
+                        <button className="input-group-text" id="">Upload</button>
+                      </div> */}
+                    </div>
+
+
                     </div>
 
                     <div className="form-group">
